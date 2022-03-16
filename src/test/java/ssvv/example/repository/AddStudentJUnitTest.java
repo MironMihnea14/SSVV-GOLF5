ec1e362c81a2dd3297c59c167eb89890b173a52c
MironMihnea14 <mironmihnea14@gmail.com>
2022-03-16 19:48:09 +0200

    meaningfull message

--- src/test/java/ssvv/example/repository/AddStudentJUnitTest.java
+++ src/test/java/ssvv/example/repository/AddStudentJUnitTest.java
@@ -9,13 +9,9 @@ import validation.StudentValidator;
 public class AddStudentJUnitTest {
     StudentXMLRepository studentXMLRepository;
 
-    @Before
-    public void init() {
-        studentXMLRepository = new StudentXMLRepository(new StudentValidator(), "studenti.xml");
-    }
-
     @Test
     public void addStudentTest() {
+        studentXMLRepository = new StudentXMLRepository(new StudentValidator(), "studenti.xml");
         Student studentToBeAdded = new Student("14", "altNumeNou", 935);
 
         Student addedStudent = this.studentXMLRepository.save(studentToBeAdded);
